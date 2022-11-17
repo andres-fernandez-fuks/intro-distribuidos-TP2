82f6254bc072915f9a50bb1891f28ca728532f3e
Sebastián Capelli <scapelli@fi.uba.ar>
2022-11-17 12:56:27 -0300

    relative paths in scripts

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-export POX_FOLDER_PATH="/home/andy/pox"
+export POX_FOLDER_PATH="/home/$USER/pox"
 yes | cp -rf firewall.py settings.py block_rule.py $POX_FOLDER_PATH/pox/misc////
 gnome-terminal -- /bin/bash -c 'cd $POX_FOLDER_PATH && ./pox.py misc.firewall forwarding.l2_learning; bash'
 sleep 2
