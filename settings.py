b75180992029696135e403bf93a9169455453da0
Agustin Lopez Nuñez <alopezn@fi.uba.ar>
2022-11-20 16:53:07 -0300

    fix src addr on rule 2

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ FIREWALL_SWITCH_ID = 1
 
 # Variables referidas a reglas solicitadas
 R1_BLOCKED_DST_PORT = 80
-R2_BLOCKED_SRC_ADDR = 2
+R2_BLOCKED_SRC_ADDR = 1
 R2_BLOCKED_DST_PORT = 5001
 R2_BLOCKED_PROTOCOL = pkt.ipv4.UDP_PROTOCOL
 R3_BLOCKED_1_ADDR = 2
@@ -25,13 +25,13 @@ rule_2_restrictions = {
 rule_3_restrictions = {
     "src_host": R3_BLOCKED_1_ADDR,
     "dst_host": R3_BLOCKED_2_ADDR,
-    "src_port": 80, # no se usa como src_port, se usa como dst_host para el trafico que va de ADDR2 a ADDR1
+    "src_port": 80,  # no se usa como src_port, se usa como dst_host para el trafico que va de ADDR2 a ADDR1
     "dst_port": 5001,
     "protocol": pkt.ipv4.UDP_PROTOCOL,
 }
 
 ACTIVE_RULES = [
-    # br.GenericBlockRule(rule_1_restrictions),
+    br.GenericBlockRule(rule_1_restrictions),
     br.GenericBlockRule(rule_2_restrictions),
     # br.GenericBlockRule(rule_3_restrictions),
 ]
