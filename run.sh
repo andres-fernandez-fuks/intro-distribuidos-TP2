83ae5ad48f704dc431bf373cb379b1d9552bfda2
Andy <48918488+andres-fernandez-fuks@users.noreply.github.com>
2022-11-21 20:41:51 -0300

    adds parameter to run script

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ export POX_FOLDER_PATH="/home/$USER/pox"
 yes | cp -rf firewall.py settings.py block_rule.py $POX_FOLDER_PATH/pox/misc////
 gnome-terminal -- /bin/bash -c 'cd $POX_FOLDER_PATH && ./pox.py misc.firewall forwarding.l2_learning; bash'
 sleep 2
-sudo mn --custom ./topology.py --topo custom,4 --mac --controller remote
+sudo mn --custom ./topology.py --topo custom,$1 --mac --controller remote
