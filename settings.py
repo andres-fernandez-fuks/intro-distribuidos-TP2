cc30472048ff7835132b4fee63a7a0927139853b
Andy <48918488+andres-fernandez-fuks@users.noreply.github.com>
2022-11-21 23:24:31 -0300

    refactors settings

--- settings.py
+++ settings.py
@@ -7,63 +7,39 @@ from block_rule import (
 )
 
 # Esta variable indica que switch aplicara el firewall
-FIREWALL_SWITCH_ID = 4
-
-# Variables referidas a reglas por defecto
-R1_BLOCKED_DST_PORT = 80
-R2_BLOCKED_SRC_ADDR = 1
-R2_BLOCKED_DST_PORT = 5001
-R2_BLOCKED_PROTOCOL = pkt.ipv4.UDP_PROTOCOL
-R3_BLOCKED_1_ADDR = 2
-R3_BLOCKED_2_ADDR = 4
+FIREWALL_SWITCH_ID = 1
 
 # Definiciones de las reglas por defecto, con sus campos respectivos
+
 rule_1_restrictions = {
-    "dst_port": R1_BLOCKED_DST_PORT,
+    "dst_port": 80,
 }
 
 rule_2_restrictions = {
-    "src_host": R2_BLOCKED_SRC_ADDR,
-    "dst_port": R2_BLOCKED_DST_PORT,
-    "protocol": R2_BLOCKED_PROTOCOL,
+    "src_host": 1,
+    "dst_port": 5001,
+    "protocol": pkt.ipv4.UDP_PROTOCOL,
 }
 
 rule_3_restrictions = {
-    "src_host": R3_BLOCKED_1_ADDR,  # No importa cual es src y cual es dst
-    "dst_host": R3_BLOCKED_2_ADDR,
+    "src_host": 2,  # No importa cual es src y cual es dst
+    "dst_host": 4,
 }
 
 # Reglas extra
-extra_rule_1 = {
-    "dst_port": R1_BLOCKED_DST_PORT,
-    "src_port": 81,
-}  # funciona
 
-extra_rule_2 = {
+extra_rule_restrictions = {
     "src_host": 1,
-    "dst_port": 80,
-    "src_port": 81,
-    "protocol": pkt.ipv4.TCP_PROTOCOL,
-}  # funciona
-
-extra_rule_3 = {
-    "src_host": R2_BLOCKED_SRC_ADDR,
-    "dst_port": R2_BLOCKED_DST_PORT,
-    "src_port": 8000,
-    "protocol": R2_BLOCKED_PROTOCOL,
-}
-
-extra_rule_4 = {
-    "src_host": R3_BLOCKED_1_ADDR,
-    "dst_host": R3_BLOCKED_2_ADDR,
+    "dst_host": 2,
+    "src_port": 5001,
     "dst_port": 8001,
-    "protocol": pkt.ipv4.ICMP_PROTOCOL,
 }
 
+extra_rule = GenericBlockRule(extra_rule_restrictions)
 
 ACTIVE_RULES = [
-    # GenericBlockRule(extra_rule_4),
-    BlockRuleTypeI()
+    # BlockRuleTypeI(),
     # BlockRuleTypeII(),
     # BlockRuleTypeIII(),
+    extra_rule,
 ]
